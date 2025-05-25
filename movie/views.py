7f7c1d120c4fbed5714e41fe899054adb771988c
Johncakes <johnnygamesover9000@gmail.com>
2025-05-25 16:58:29 +0900

    Fix : Change movie card description to db info

--- movie/views.py
+++ movie/views.py
@@ -4,6 +4,9 @@ from django.views.decorators.http import require_POST
 from movie.models import Review
 from movie.models.movie import Movie
 
+def index(request):
+    movie = Movie.objects.all()
+    return render(request, 'index.html', {'movie': movie})
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
