8d5c844fb4a0d4c12a2308234f30aced679bc64a
Johncakes <johnnygamesover9000@gmail.com>
2025-05-25 16:58:29 +0900

    Fix : Change movie card description to db info

--- movie/urls.py
+++ movie/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from config.views import index
+from movie.views import index
 from movie import views
 
 urlpatterns = [
--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from movie.models.movie import Movie
 
+def index(request):
+    movie = Movie.objects.all()
+    return render(request, 'index.html', {'movie': movie})
+
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'feature_detail_page.html', {'movie': movie})
+
