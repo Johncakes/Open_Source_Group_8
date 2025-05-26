ed4803ae361983c22a5605c9fd22dfbf27960516
rukigame <kimjin2002002@gmail.com>
2025-05-26 15:59:22 +0900

    feat: search 이미지/나열순서
    이미지 출력되게 하고
    최신순 인기순으로 나열되게 함

--- movie/urls.py
+++ movie/urls.py
@@ -5,4 +5,5 @@ from movie import views
 urlpatterns = [
     path('', index, name='index'),
     path("movies/<int:movie_id>/", views.movie_detail, name='movie_detail'),
+    path('search/', views.movie_search, name='movie_search'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,27 @@ from movie.models.movie import Movie
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'feature_detail_page.html', {'movie': movie})
+
+def movie_search(request):
+    query = request.GET.get('q', '')
+    field = request.GET.get('field', 'title')
+    sort = request.GET.get('sort', 'latest')
+    #results = []
+    results = Movie.objects.all()  # 기본: 전체에서 시작
+
+    if query:
+        if field == 'title':
+            results = Movie.objects.filter(title__icontains=query)
+        elif field == 'original_title':
+            results = Movie.objects.filter(original_title__icontains=query)
+    if sort == 'latest':
+        results = Movie.objects.order_by('-release_date')
+    elif sort == 'popular':
+        results = Movie.objects.order_by('-popularity')
+
+    return render(request, 'search.html', {
+        'query': query,
+        'field': field,
+        'sort': sort,
+        'results': results
+    })
