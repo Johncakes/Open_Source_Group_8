35bd50b17d60d6661e3c9c9676d2159869a4d429
bugogi <gjtmddn0124@gmail.com>
2025-05-27 01:20:13 +0900

    feat: add reviews, add avg rating, add 'average_rating' on views.py

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from movie.models.movie import Movie
+from django.db.models import Avg
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
-    return render(request, 'feature_detail_page.html', {'movie': movie})
+    average_rating = movie.reviews.aggregate(avg=Avg('rating'))['avg']
+    return render(request, 'feature_detail_page.html', {
+        'movie': movie,
+        'average_rating': average_rating
+    })
