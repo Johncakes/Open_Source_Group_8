cfe988bc44ab2c069bdd4b05ed1b19859d1a71d7
Johncakes <johnnygamesover9000@gmail.com>
2025-05-28 16:33:42 +0900

    fix : remove duplicate function

--- movie/views.py
+++ movie/views.py
@@ -11,12 +11,6 @@ def index(request):
     random_movie = movie.order_by('?')[:30]
     return render(request, 'index.html', {'movie': movie, 'popular_movie': popular_movie, 'random_movie': random_movie})
 
-def index(request):
-    movie = Movie.objects.all()
-    popular_movie = movie.order_by('-popularity')[:30]
-    random_movie = movie.order_by('?')[:30]
-    return render(request, 'index.html', {'movie': movie, 'popular_movie': popular_movie, 'random_movie': random_movie})
-
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     average_rating = movie.reviews.aggregate(avg=Avg('rating'))['avg']
