0818341b21dafeae0adea120be5e3cf130e1b5d8
rukigame <kimjin2002002@gmail.com>
2025-05-28 16:03:55 +0900

    feat: 선택검색 삭제/ 카드 배열조정
    검색할 단어의 종류 선택을 삭제하고 화면 크기에 따라 카드 배열 조정

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from movie.models.movie import Movie
+from django.db.models import Q  # 이거 추가
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
@@ -7,24 +8,23 @@ def movie_detail(request, movie_id):
 
 def movie_search(request):
     query = request.GET.get('q', '')
-    field = request.GET.get('field', 'title')
+
     sort = request.GET.get('sort', 'latest')
     #results = []
     results = Movie.objects.all()  # 기본: 전체에서 시작
 
     if query:
-        if field == 'title':
-            results = Movie.objects.filter(title__icontains=query)
-        elif field == 'original_title':
-            results = Movie.objects.filter(original_title__icontains=query)
+        results = results.filter(
+            Q(title__icontains=query) | Q(original_title__icontains=query)
+        )
     if sort == 'latest':
-        results = Movie.objects.order_by('-release_date')
+        results = results.order_by('-release_date')
     elif sort == 'popular':
-        results = Movie.objects.order_by('-popularity')
+        results = results.order_by('-popularity')
 
     return render(request, 'search.html', {
         'query': query,
-        'field': field,
+
         'sort': sort,
         'results': results
     })
