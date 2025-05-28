63040edc11e9ea9ba84f1e1a48843cf03591befb
toby <dlwodnjs724@gmail.com>
2025-05-28 15:19:20 +0900

    feat: review model 추가

--- movie/models/__init__.py
+++ movie/models/__init__.py
@@ -1,5 +1,6 @@
 from movie.models.genre import Genre
 from movie.models.movie import Movie, MovieCast, MovieCrew
+from movie.models.review import Review
 
 
-__all__ = ["Genre", "Movie", "MovieCast", "MovieCrew"]
+__all__ = ["Genre", "Movie", "MovieCast", "MovieCrew", "Review"]
