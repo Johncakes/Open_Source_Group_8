be18b9a7cbbc01e8671b5ddb2a9c82e91d91fc25
rukigame <kimjin2002002@gmail.com>
2025-05-28 16:21:28 +0900

    feat: pagination 적용 중

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from movie.models.movie import Movie
-from django.db.models import Q  # 이거 추가
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
@@ -21,6 +22,10 @@ def movie_search(request):
         results = results.order_by('-release_date')
     elif sort == 'popular':
         results = results.order_by('-popularity')
+
+    paginator = Paginator(results, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request, 'search.html', {
         'query': query,
