8ae10081c2eb2268c00b5673ede32d7054d15157
Johncakes <johnnygamesover9000@gmail.com>
2025-05-28 15:49:38 +0900

    refactor : index page code refactoring

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,9 @@ from movie.models.movie import Movie
 
 def index(request):
     movie = Movie.objects.all()
-    return render(request, 'index.html', {'movie': movie})
+    popular_movie = movie.order_by('-popularity')[:30]
+    random_movie = movie.order_by('?')[:30]
+    return render(request, 'index.html', {'movie': movie, 'popular_movie': popular_movie, 'random_movie': random_movie})
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
